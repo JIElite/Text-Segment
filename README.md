515a9ccbc6943b73c0d689f396bffa7a14dc6730
JIElite <jielite.tw@gmail.com>
2017-11-05 02:50:43 +0800

    update

---
+++ README.md
@@ -0,0 +1,45 @@
+# 分詞系統 (Segment System)
+- using simple Trie data structure
+- maximum matching
+
+## Dependency Intallation
+- Python 3.5
+```
+# we using virtuel environment
+$ virtuelenv -p python3.5 env
+$ source env/bin/activate
+(env)$ pip install -r requirements.txt
+```
+
+## Example
+
+```python
+import segment
+
+# 創建你的字典或是載入先前的字典
+dictionary = dictionary.Dictionary()
+dictionary.load([optional_path])
+
+# 建立斷詞系統，並且可以搭配不同的字典來使用
+segments_agent = segment.SegmentSystem()
+segments_agent.use_dictionary(dictionary)
+
+context = '''勞神勞力又勞心勞費的勞什骨子喜歡勞什子嗎？'''
+segment_list = segments_agent.segments(context)
+print(segment_list)
+# result: ['勞神', '勞力', '又', '勞心', '勞費', '的', '勞什骨子', '喜歡', '勞什子', '嗎', '？']
+```
+
+## How to use?
+- clone this project
+- cd /src
+- execute main.py
+```
+1. Simple input:
+(env)$ python main.py -s '今天天氣好冷，這種天氣就想睡覺。睡眠不足的話，會氣噗噗'
+# ['今天', '天氣', '好', '冷', '，', '這', '種', '天氣', '就', '想', '睡覺', '。', '睡眠', '不足', '的', '話', '，', '會', '氣', '噗', '噗']
+
+2. File Input:
+(env)$ python main.py -f context.txt
+# ['今天', '早上', '在', '操場', '看', '到', '一個', '身形', '疲累', '的', '人', '，', '被', '別人', '稱', '作', '勞什骨子', '。', '到底', '勞什骨子', '是', '什麼', '呢', '？']
+```
--- src/main.py
+++ src/main.py
@@ -1,22 +1,25 @@
+import argparse 
 import segment
+import dictionary
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="using input context file to segment")
+parser.add_argument("-s", "--simple", help="just add simple context to end of CLI to segment")
+args = parser.parse_args()
 
+dictionary = dictionary.Dictionary()
+dictionary.load()
 
-if __name__ == '__main__':
-    dict_1 = '../dictionray/dict_revised_2015_20160523/dict_revised_2015_20160523_1.xls'
-    dict_2 = '../dictionray/dict_revised_2015_20160523/dict_revised_2015_20160523_2.xls'
-    dict_3 = '../dictionray/dict_revised_2015_20160523/dict_revised_2015_20160523_3.xls'
-    
-    dictionary = segment.Dictionary()
-    dictionary.load()
-    # dictionary.expand_dictionary_by_xls(dict_1)
-    # dictionary.expand_dictionary_by_xls(dict_2)
-    # dictionary.expand_dictionary_by_xls(dict_3)
-    # dictionary.save()
+segments_agent = segment.SegmentSystem()
+segments_agent.use_dictionary(dictionary)
 
-    segments_agent = segment.SegmentSystem()
-    segments_agent.use_dictionary(dictionary)
-
-    context = '''勞神勞力又勞心勞費的勞什骨子喜歡勞什子嗎？'''
-    segment_list = segments_agent.segments(context)
-    print(segment_list)
+if args.file:
+    with open(args.file, 'r') as fd:
+        for context in fd.readlines():
+            print(segments_agent.segments(context))
+elif args.simple:
+    context = args.simple
+    print(segments_agent.segments(context))
+else:
+    print("No Input Context")
+    print("If you need any help, please try: python main.py -h")
